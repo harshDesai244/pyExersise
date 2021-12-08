0de7e1c489a1c1d17ba69f2aae7703e75e4f1387
harshDesai244 <harshdesai244@gmail.com>
2021-12-08 12:18:01 +0100

    same

---
+++ Exe.py
@@ -0,0 +1,12 @@
+Sentence = ("This is a common interview question")
+
+char_frequency = {}
+for char in Sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+char_frequency_sorted = sorted(char_frequency.items(),
+                               key=lambda kv: kv[1], reverse=True)
+
+print(char_frequency_sorted[0])
